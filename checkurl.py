d488ad7d687d76afccb0f08878cae8ea1d174de8
Alvphil <philip.nyblom@gmail.com>
2023-11-01 23:45:09 +0100

    threading included

--- checkurl.py
+++ checkurl.py
@@ -1,6 +1,7 @@
-import requests
+import requests, concurrent.futures
 import os
 import urllib3
+
 
 urllib3.disable_warnings()
 
@@ -10,14 +11,33 @@ if os.path.exists("statuscodes.txt"):
 with open(r"complete_list.txt", 'r') as file:
 	urls = file.read().splitlines()
 
-for url in urls:
+def url_checker(url):
     print(f"checking URL: {url}")
     try: 
-        response = requests.get('https://' + url, allow_redirects=True, timeout=10, verify=False)
+        response = requests.get('https://' + url, allow_redirects=True, timeout=5, verify=False)
     except requests.exceptions.RequestException:
         try: 
             response = requests.get('http://' + url, allow_redirects=True, timeout=10)
         except requests.exceptions.RequestException:
-             continue
+             return
     with open(r"statuscodes.txt", 'a') as outFile:
         outFile.write(f"{str(response.status_code)};{url}\n")
+def main():
+    #for url in urls:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as exeutor:
+         exeutor.map(url_checker, urls)
+         #url_checker(url)
+
+    
+    #print(f"checking URL: {url}")
+    #try: 
+    #    response = requests.get('https://' + url, allow_redirects=True, timeout=5, verify=False)
+    #except requests.exceptions.RequestException:
+    #    try: 
+    #        response = requests.get('http://' + url, allow_redirects=True, timeout=10)
+    #    except requests.exceptions.RequestException:
+    #         continue
+    #with open(r"statuscodes.txt", 'a') as outFile:
+    #    outFile.write(f"{str(response.status_code)};{url}\n")
+
+main()
