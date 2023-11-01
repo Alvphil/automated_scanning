39920415a238702978fed1390c1fafe616c32eee
Alvphil <philip.nyblom@gmail.com>
2023-11-02 00:05:05 +0100

    Clean up of some files, and added sorting

--- checkurl.py
+++ checkurl.py
@@ -7,7 +7,7 @@ if os.path.exists("statuscodes.txt"):
     os.remove("statuscodes.txt")
 
 with open(r"complete_list.txt", 'r') as file:
-	urls = file.read().splitlines()
+    urls = file.read().splitlines()
 
 # url_checker checks the respons from sites in the complete_list and writes the status code it receives back from the server to a file.
 def url_checker(url):
@@ -19,13 +19,24 @@ def url_checker(url):
             response = requests.get('http://' + url, allow_redirects=True, timeout=10)
         except requests.exceptions.RequestException:
              return
-    with open(r"statuscodes.txt", 'a') as outFile:
+    with open(r"statuscodes_u.txt", 'a') as outFile:
         outFile.write(f"{str(response.status_code)};{url}\n")
+
+def sort_file():
+    with open(r"statuscodes_u.txt", 'r') as file:
+        urls = file.read().splitlines()
+    sorted_urls = sorted(urls)
+    for url in sorted_urls:
+        with open(r"statuscodes.txt", 'a') as outFile:
+            outFile.write(f"{url}\n")
+    if os.path.exists("statuscodes_u.txt"):
+        os.remove("statuscodes_u.txt")
 
 def main():
     # using multiple threads to look through the list of domains. 
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as exeutor:
          exeutor.map(url_checker, urls)
+    sort_file()
 
     
 
